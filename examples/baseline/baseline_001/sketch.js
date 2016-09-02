4c21e1d99356efc00d5607993eb5b2aa68fdd54f
chrischne <chrischne@gmail.com>
2016-09-02 22:27:51 +0200

    record delta, theta, alpha, beta, gamma

--- examples/baseline/baseline_001/sketch.js
+++ examples/baseline/baseline_001/sketch.js
@@ -1,9 +1,28 @@
+
+//TODO 
+//nice header here
+//make recorder object for the data, that will make it simpler for the students
 var muse;
 
 
+//horseShoe
 var horseShoeData = [];
 var horseShoeStatus = [0, 0, 0, 0];
 
+//delta, theta, alpha, beta, gamma
+var deltaData = [];
+var thetaData = [];
+var alphaData = [];
+var betaData = [];
+var gammaData = [];
+
+var deltaStatus = [0,0,0,0];
+var thetaStatus = [0,0,0,0];
+var alphaStatus = [0,0,0,0];
+var betaStatus = [0,0,0,0];
+var gammaStatus = [0,0,0,0];
+
+//recording specific booleans
 var isRecording = false;
 var dataReady = false;
 
@@ -138,29 +157,80 @@ function mineData() {
 	console.log('horseShoeStatus', horseShoeStatus);
 
 
+	//mine delta, theta, alpha, beta, gamma
+	//calculate the average per sensor
+	deltaStatus = calcAvg(deltaData);
+	console.log('deltaStatus',deltaStatus);
+
+	thetaStatus = calcAvg(thetaData);
+	console.log('thetaStatus',thetaStatus);
+
+	alphaStatus = calcAvg(alphaData);
+	console.log('alphaStatus',alphaStatus);
+
+	betaStatus = calcAvg(betaData);
+	console.log('betaStatus',betaStatus);
+
+	gammaStatus = calcAvg(gammaData);
+	console.log('gammaStatus',gammaStatus);
+
+
+
 	dataReady = true;
 
 
 
 }
 
+function calcAvg(arr){
+	//[[a,b,c,d],[a,b,c,d],...]
+
+	var avgArr = [];
+	//loop through sensors
+	for(var i=0; i<4; i++){
+
+		var sum = 0;
+		//loop through recorded data
+		for(var j=0; j<arr.length; j++){
+			sum += arr[j][i];
+		}
+
+		var avg = sum/arr.length;
+
+		avgArr.push(avg);
+	}
+
+	return avgArr;
+}
+
 function parseDelta(msg) {
+	var values = msg.slice(2);
+	//console.log('values', values);
+	deltaData.push(values);
 
 }
 
 function parseTheta(msg) {
-
+var values = msg.slice(2);
+	//console.log('values', values);
+	thetaData.push(values);
 }
 
 function parseAlpha(msg) {
-
+	var values = msg.slice(2);
+	//console.log('values', values);
+	alphaData.push(values);
 
 }
 
 function parseBeta(msg) {
-
+var values = msg.slice(2);
+	//console.log('values', values);
+	betaData.push(values);
 }
 
 function parseGamma(msg) {
-
+var values = msg.slice(2);
+	//console.log('values', values);
+	gammaData.push(values);
 }
